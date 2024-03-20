1a9bec3077afede29beda7d36a43cccd5bb8af82
psj <sangjin.park@cjconnect.io>
2024-03-20 16:42:31 +0900

    기본적인 설정 추가

--- infrastructures/network/src/main/kotlin/com/github/silbaram/infrastructures/socket/NetworkScan.kt
+++
@@ -1,3 +0,0 @@
-package com.github.silbaram.infrastructures.socket
-
-interface NetworkScan
---
+++ infrastructures/network/src/main/kotlin/com/github/silbaram/infrastructures/socket/annotation/EnableTcpServer.kt
@@ -0,0 +1,5 @@
+package com.github.silbaram.infrastructures.socket.annotation
+
+@Target(AnnotationTarget.TYPE)
+@Retention(AnnotationRetention.RUNTIME)
+annotation class EnableTcpServer
